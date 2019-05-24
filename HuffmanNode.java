4ff319c57083eb3f915397e09a9b75b9937fc15e
Helen <hbelete13@gmail.com>
2019-05-24 12:42:43 -0700

    Update HuffmanNode.java

--- HuffmanNode.java
+++ HuffmanNode.java
@@ -1,27 +1,31 @@
-// This is the node class for the HuffmanNode program
+// This is the node class for the HuffmanNode program. This creates a HuffmanNode
+// object.
 
 import java.io.*;
 import java.util.*;
 
 public class HuffmanNode implements Comparable<HuffmanNode> {
-
+   // either null or represents a character -left pointer
    public HuffmanNode left;
+   // either null or represents a character
    public HuffmanNode right;
+   // ascii of the character or null (for example, "a"'s ascii is 097
    public int data;
-   public int chars;
+   // how often character shows up (count)
+   public int freq;
 
-   public HuffmanNode(int data, int chars) {
-      this(data, chars, null, null);
+   public HuffmanNode(int freq, int data) {
+      this(freq, data, null, null);
    }
 
-   public HuffmanNode(int data, int chars, HuffmanNode left, HuffmanNode right) {
+   public HuffmanNode(int freq, int data, HuffmanNode left, HuffmanNode right) {
+      this.freq = freq;
       this.data = data;
-      this.chars = chars;
       this.left = left;
       this.right = right;
    }
 
    public int compareTo(HuffmanNode other) {
-      return this.data-other.data;
+      return this.freq-other.freq;
    }
 }
